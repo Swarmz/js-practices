872b756f7c0e336ffe0631bb6856ed8695c58ea6
Swarmz <gavinsutton6511@gmail.com>
2026-05-27 14:29:45 +0900

    Initial commit

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,14 @@
+var num = 0;
+
+while (num < 20) {
+  num++;
+  if (num % 5 == 0 && num % 3 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 5 == 0) {
+    console.log("Buzz");
+  } else if (num % 3 == 0) {
+    console.log("Fizz");
+  } else {
+    console.log(`${num}`);
+  }
+}
