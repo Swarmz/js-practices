0e68f7bf2d5a2e1067c27fc1550c530d1ddee6cb
Swarmz <gavinsutton6511@gmail.com>
2026-06-29 13:23:51 +0900

    比較演算子を==から===に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,11 +2,11 @@ let num = 0;
 
 while (num < 20) {
   num++;
-  if (num % 5 == 0 && num % 3 == 0) {
+  if (num % 5 === 0 && num % 3 === 0) {
     console.log("FizzBuzz");
-  } else if (num % 5 == 0) {
+  } else if (num % 5 === 0) {
     console.log("Buzz");
-  } else if (num % 3 == 0) {
+  } else if (num % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(`${num}`);
