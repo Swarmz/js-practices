9006ad38818e02962489cdc8a4e6d4fca51c45ca
Swarmz <gavinsutton6511@gmail.com>
2026-06-29 13:09:40 +0900

    カレンダーのプログラムを実装

---
+++ 02.calendar/cal.js
@@ -0,0 +1,33 @@
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+const today = new Date();
+const month = args.m ?? today.getMonth() + 1;
+const year = args.y ?? today.getFullYear();
+
+const firstDay = new Date(year, month - 1, 1);
+const lastDay = new Date(year, month, 0);
+
+const calendarDays = [];
+
+for (
+  let day = new Date(firstDay);
+  day <= lastDay;
+  day.setDate(day.getDate() + 1)
+) {
+  calendarDays.push(new Date(day));
+}
+const yearMonth = `${month}月 ${year}`;
+
+console.log(yearMonth.padStart((20 + yearMonth.length) / 2));
+console.log("日 月 火 水 木 金 土");
+
+process.stdout.write("   ".repeat(firstDay.getDay()));
+calendarDays.forEach((day) => {
+  if (day.getDay() == 0) {
+    process.stdout.write("\n");
+  }
+  process.stdout.write(`${String(day.getDate()).padStart(2, " ")} `);
+});
+process.stdout.write("\n");
