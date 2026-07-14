946b1b865f9ae1a694f1897e15de788fc3ab6e9a
Swarmz <gavinsutton6511@gmail.com>
2026-07-14 09:56:21 +0900

    カレンダー出力の行末の空白を削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -22,7 +22,12 @@ for (
   day <= lastDay;
   day.setDate(day.getDate() + 1)
 ) {
-  process.stdout.write(`${String(day.getDate()).padStart(2, " ")} `);
+  process.stdout.write(`${String(day.getDate()).padStart(2, " ")}`);
+  
+  if (day.getDay() !== 6 && day.getDate() !== lastDay.getDate()) {
+    process.stdout.write(" ");
+  }
+
   if (day.getDay() === 6) {
     process.stdout.write("\n");
   }
