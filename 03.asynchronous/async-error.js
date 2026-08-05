85f55aee40c5ce3b95f481779969c7d08413feff
gavin <gavinsutton6511@gmail.com>
2026-08-05 10:57:44 +0900

    Top-level await を使い async function main の定義を削除

--- 03.asynchronous/async-error.js
+++ 03.asynchronous/async-error.js
@@ -3,32 +3,28 @@
 import { db } from "./db.js";
 import { run, get, close } from "./sqlite-promises.js";
 
-async function main() {
-  try {
-    await run(
-      db,
-      `
-      CREATE TABLE books (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL UNIQUE
-      )`,
-    );
+try {
+  await run(
+    db,
+    `
+    CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+    )`,
+  );
 
-    const result = await run(db, "INSERT INTO books (title) VALUES NULL");
-    console.log(result.lastID);
-  } catch (err) {
-    console.error(err);
-  }
-
-  try {
-    const row = await get(db, "SELECT author FROM books");
-    console.log(row);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await run(db, "DROP TABLE books");
-    await close(db);
-  }
+  const result = await run(db, "INSERT INTO books (title) VALUES NULL");
+  console.log(result.lastID);
+} catch (err) {
+  console.error(err);
 }
 
-main();
+try {
+  const row = await get(db, "SELECT author FROM books");
+  console.log(row);
+} catch (err) {
+  console.error(err);
+} finally {
+  await run(db, "DROP TABLE books");
+  await close(db);
+}
--- 03.asynchronous/async-success.js
+++ 03.asynchronous/async-success.js
@@ -3,32 +3,28 @@
 import { db } from "./db.js";
 import { run, get, close } from "./sqlite-promises.js";
 
-async function main() {
-  try {
-    await run(
-      db,
-      `
-      CREATE TABLE books (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL UNIQUE
-      )`,
-    );
+try {
+  await run(
+    db,
+    `
+    CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE
+    )`,
+  );
 
-    const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
-      "Alice in Wonderland",
-    ]);
-    console.log(result.lastID);
+  const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
+    "Alice in Wonderland",
+  ]);
+  console.log(result.lastID);
 
-    const row = await get(db, "SELECT * FROM books WHERE id = ?", [
-      result.lastID,
-    ]);
-    console.log(row);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await run(db, "DROP TABLE books");
-    await close(db);
-  }
+  const row = await get(db, "SELECT * FROM books WHERE id = ?", [
+    result.lastID,
+  ]);
+  console.log(row);
+} catch (err) {
+  console.error(err);
+} finally {
+  await run(db, "DROP TABLE books");
+  await close(db);
 }
-
-main();
