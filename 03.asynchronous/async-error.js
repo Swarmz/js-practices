5ae1a2cd2bcb0031353eb5085968579754d74a38
Swarmz <gavinsutton6511@gmail.com>
2026-08-10 15:05:47 +0900

    想定するエラーのみを捕捉するよう err.code で判定

--- 03.asynchronous/async-error.js
+++ 03.asynchronous/async-error.js
@@ -13,14 +13,22 @@ try {
     const result = await run(db, "INSERT INTO books (title) VALUES (NULL)");
     console.log(result.lastID);
   } catch (err) {
-    console.error(err);
+    if (err.code === "SQLITE_CONSTRAINT") {
+      console.error(err);
+    } else {
+      throw err;
+    }
   }
 
   try {
     const row = await get(db, "SELECT author FROM books");
     console.log(row);
   } catch (err) {
-    console.error(err);
+    if (err.code === "SQLITE_ERROR") {
+      console.error(err);
+    } else {
+      throw err;
+    }
   }
 
   await run(db, "DROP TABLE books");
