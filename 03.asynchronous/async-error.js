55cd84f5f74051aeeb67d4ce874d8093bca7608c
Swarmz <gavinsutton6511@gmail.com>
2026-08-10 10:52:14 +0900

    例外を捕捉する範囲を限定

--- 03.asynchronous/async-error.js
+++ 03.asynchronous/async-error.js
@@ -13,18 +13,21 @@ try {
     )`,
   );
 
-  const result = await run(db, "INSERT INTO books (title) VALUES NULL");
-  console.log(result.lastID);
-} catch (err) {
-  console.error(err);
-}
+  try {
+    const result = await run(db, "INSERT INTO books (title) VALUES NULL");
+    console.log(result.lastID);
+  } catch (err) {
+    console.error(err);
+  }
 
-try {
-  const row = await get(db, "SELECT author FROM books");
-  console.log(row);
-} catch (err) {
-  console.error(err);
-} finally {
+  try {
+    const row = await get(db, "SELECT author FROM books");
+    console.log(row);
+  } catch (err) {
+    console.error(err);
+  }
+
   await run(db, "DROP TABLE books");
+} finally {
   await close(db);
 }
