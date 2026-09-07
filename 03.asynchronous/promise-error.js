cedd06a057007a436c500e2aeb017a83aeb268cf
Swarmz <gavinsutton6511@gmail.com>
2026-09-07 10:30:26 +0900

    不要な then のステップを削除し、catch に統合

--- 03.asynchronous/promise-error.js
+++ 03.asynchronous/promise-error.js
@@ -10,10 +10,10 @@ run(
   .then(() => run(db, "INSERT INTO books (title) VALUES (NULL)"))
   .catch((err) => {
     console.error(err);
+    return get(db, "SELECT author FROM books");
   })
-  .then(() => get(db, "SELECT author FROM books"))
   .catch((err) => {
     console.error(err);
+    return run(db, "DROP TABLE books");
   })
-  .then(() => run(db, "DROP TABLE books"))
   .finally(() => close(db));
